6cefcf0346af5d6168cf5e0419f12f27b69acbee
Melvin Valster <melvin.valster@gmail.com>
2019-07-23 11:12:29 +0200

    Updates

---
+++ precache-manifest.b5870c5dfb96ad8e7487b04def8860cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d791dea118b58bc6aed0bfd7397c8a",
+    "url": "/wow-talent-calculator/index.html"
+  },
+  {
+    "revision": "d0168e381ec7b0897272",
+    "url": "/wow-talent-calculator/static/css/main.c3222de2.chunk.css"
+  },
+  {
+    "revision": "91133ad17b331cef2e53",
+    "url": "/wow-talent-calculator/static/js/2.b60f3740.chunk.js"
+  },
+  {
+    "revision": "d0168e381ec7b0897272",
+    "url": "/wow-talent-calculator/static/js/main.3f32f54d.chunk.js"
+  },
+  {
+    "revision": "fbb537f9cfa0840057ae",
+    "url": "/wow-talent-calculator/static/js/runtime~main.765bec70.js"
+  }
+]);
